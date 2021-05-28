75b00093eb2a51434270557dffaf762ff5c306c8
FranciscoBraaz <francisco_braaz@hotmail.com>
2021-05-27 23:04:58 -0300

    Criando rota e componente de perfil do usuário

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import Photo from "./Components/Photo/Photo";
 import User from "./Components/User/User";
+import UserProfile from "./Components/User/UserProfile";
 import { UserStorage } from "./contexts/UserContext";
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <ProtectedRoute path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
